c862c1553096b2b17d694d8392f253517cb93188
Infin8gamer1 <jacob@mjhsoftware.com>
2018-03-21 17:25:36 -0700

    Did something to auto pos1 path 1

--- src/org/usfirst/frc4089/Stealth2018/autoCommands/Position1Path1.java
+++ src/org/usfirst/frc4089/Stealth2018/autoCommands/Position1Path1.java
@@ -85,7 +85,7 @@ public class Position1Path1 extends CommandGroup {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return RobotMap.isAutoFinished;
+	  return RobotMap.isAutoFinished;
   }
 
   // Called once after isFinished returns true
